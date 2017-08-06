711d1ba45935524d4f29a301f2d7781efb016315
mali <sujonpueee@gmail.com>
2017-08-06 19:36:48 -0400

    This is my mobile project

---
+++ src/apples.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class apples {
+
+	public static void main(String[] args) {
+		Scanner bucky = new Scanner(System.in);
+		
+		
+		int tuna = 5;
+		int bass = 18;
+		++tuna;
+		++bass;
+		
+		
+		tuna -= tuna + 5;
+		System.out.println(tuna);
+		
+
+	}
+
+}
